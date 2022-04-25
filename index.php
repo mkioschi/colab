8a61a948b1a49f133539e45edf1ba4e623d8f93f
Mayko Kioschi <mkioschi@gmail.com>
2022-04-24 23:09:32 -0300

    Implements background video

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 <main id="the-main">
     <section class="cover">
+        <div id="video-wrapper" class="d-none d-lg-block">
+            <video poster="<?php echo get_template_directory_uri() . "/assets/img/home-1.jpg" ?>" id="video" playsinline autoplay muted loop>
+                <?php if ("computer" == "computer"): ?>
+                    <source src="<?php echo get_template_directory_uri() . "/assets/videos/background/1280x720.webm" ?>" type="video/webm">
+                <?php else: ?>
+                    <source src="<?php echo get_template_directory_uri() . "/assets/videos/background/390x800.webm" ?>" type="video/webm">
+                <?php endif; ?>
+            </video>
+        </div>
+
         <div class="mask mask-bottom">
             <img class="d-lg-none" src="<?php echo get_template_directory_uri() . "/assets/img/mask-light-mobile-bottom.svg" ?>">
             <img class="d-none d-lg-block" src="<?php echo get_template_directory_uri() . "/assets/img/mask-light-desktop-bottom.svg" ?>">
